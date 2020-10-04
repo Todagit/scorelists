94a73cc9c7299b253e527a934152745208ad3dc0
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-10-04 17:39:46 +0900

    Update 20201002101255_create_scores.rb

--- db/migrate/20201002101255_create_scores.rb
+++ db/migrate/20201002101255_create_scores.rb
@@ -3,6 +3,17 @@ class CreateScores < ActiveRecord::Migration[5.2]
     create_table :scores do |t|
 
       t.timestamps
+      t.references :user,    null:false, foreign_key: true
+      t.integer    :japanese
+      t.integer    :math1
+      t.integer    :math2
+      t.integer    :worldhistory
+      t.integer    :japanesehistory
+      t.integer    :modernsociety
+      t.integer    :geography
+      t.integer    :chemistry
+      t.integer    :physics
+      t.integer    :english
     end
   end
 end
