ea7b5d0f6da888c98272b237b07040120dc17a28
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-10-06 05:54:56 +0900

    Update 20200929095029_devise_create_users.rb

--- db/migrate/20200929095029_devise_create_users.rb
+++ db/migrate/20200929095029_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
 
-      t.string :name,               null: false
+      t.string :nickname,           null: false
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -38,6 +38,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.timestamps null: false
     end
 
+    add_index :users, :nickname,             unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
