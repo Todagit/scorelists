ab1ebb87a2e90ce3bec39447ebdcc634cda7747f
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-10-12 13:54:36 +0900

    create study_score.rb

---
+++ app/models/study_score.rb
@@ -0,0 +1,2 @@
+class StudyScore < ApplicationRecord
+end
---
+++ test/models/study_score_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StudyScoreTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
