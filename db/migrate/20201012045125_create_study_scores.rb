b0708e4e60282b3813d764a63c54cdd13f5bce8b
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-10-17 06:28:45 +0900

    Update 20201012045125_create_study_scores.rb

--- db/migrate/20201012045125_create_study_scores.rb
+++ db/migrate/20201012045125_create_study_scores.rb
@@ -4,16 +4,19 @@ class CreateStudyScores < ActiveRecord::Migration[5.2]
 
       t.timestamps
       t.references :user,    null:false, foreign_key: true
+      t.string     :test_name
+      t.date       :test_date
       t.integer    :japanese
-      t.integer    :math1
-      t.integer    :math2
+      t.integer    :math1a
+      t.integer    :math2b
       t.integer    :worldhistory
       t.integer    :japanesehistory
       t.integer    :modernsociety
       t.integer    :geography
       t.integer    :chemistry
       t.integer    :physics
-      t.integer    :english
+      t.integer    :english_l
+      t.integer    :english_r
     end
   end
 end
