b3e649ab94beb19fd189d26e66187e875e5c5f42
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-10-17 06:29:08 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_02_101255) do
+ActiveRecord::Schema.define(version: 2020_10_12_045125) do
 
   create_table "scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,26 @@ ActiveRecord::Schema.define(version: 2020_10_02_101255) do
     t.bigint "user_id", null: false
     t.integer "score1"
     t.index ["user_id"], name: "index_scores_on_user_id"
+  end
+
+  create_table "study_scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.string "test_name"
+    t.date "test_date"
+    t.integer "japanese"
+    t.integer "math1a"
+    t.integer "math2b"
+    t.integer "worldhistory"
+    t.integer "japanesehistory"
+    t.integer "modernsociety"
+    t.integer "geography"
+    t.integer "chemistry"
+    t.integer "physics"
+    t.integer "english_l"
+    t.integer "english_r"
+    t.index ["user_id"], name: "index_study_scores_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -36,4 +56,5 @@ ActiveRecord::Schema.define(version: 2020_10_02_101255) do
   end
 
   add_foreign_key "scores", "users"
+  add_foreign_key "study_scores", "users"
 end
