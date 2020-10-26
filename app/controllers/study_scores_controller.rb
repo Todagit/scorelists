fa50a128b8062a5317dc1310be2c3d38c4d8ffdf
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-10-27 08:05:00 +0900

    Update study_scores_controller.rb

--- app/controllers/study_scores_controller.rb
+++ app/controllers/study_scores_controller.rb
@@ -4,6 +4,12 @@ class StudyScoresController < ApplicationController
   end
 
   def new
+    if user_signed_in?
+      @study_score = Study_score.new
+    else
+      flash[:alert] = '新しいスコアを登録するには、ログインするか新規会員登録をしてください。'
+      redirect_to root_path
+    end
   end
   
 end
