443ad17bcefebc61c610562893c7d4658cb0baec
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-11-19 12:55:08 +0900

    Update user.rb
    emailに正規表現を用いてバリデーションを設けました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname,
+    presence: true,
+    uniqueness: true
+  validates :email,
+    format: { with: /\A[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\z/},
+    presence: true
 
   has_many :scores
   has_many :study_scores
