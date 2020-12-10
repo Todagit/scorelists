73c0b8ab5fe646b44726fa38db7fbca3e37c969a
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-12-10 12:41:40 +0900

    Update toeic_score.rb

--- app/models/toeic_score.rb
+++ app/models/toeic_score.rb
@@ -5,5 +5,5 @@ class ToeicScore < ApplicationRecord
   validates  :test_date,              presence: true
   validates  :administration_number,  presence: true
   validates  :listening_score,        presence: true
-  validates  :total_score,            presence: true
+  validates  :reading_score,          presence: true
 end
