c95a7dde99434936e029492f74e76292416c31c1
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-12-10 12:27:14 +0900

    Update toeic_scores_controller.rb

--- app/controllers/toeic_scores_controller.rb
+++ app/controllers/toeic_scores_controller.rb
@@ -1,18 +1,31 @@
 class ToeicScoresController < ApplicationController
 
   def index
+
+    if user_signed_in?
+      @toeic_scores = ToeicScore.all
+      # @toeic_scores = ToeicScore.includes(:user)
+    else
+      flash[:alert] = 'スコアを閲覧するには、ログインするか新規会員登録をしてください。'
+      redirect_to root_path
+    end
+
   end
 
+
   def new
+
     if user_signed_in?
       @toeic_score = ToeicScore.new
     else
       flash[:alert] = '新しいスコアを登録するには、ログインするか新規会員登録をしてください。'
       redirect_to root_path
     end
+
   end
 
   def create
+    
   end
 
   def edit
