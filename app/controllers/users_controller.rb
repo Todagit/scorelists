3cde0f817329580b3e13b3946e5745483ee2da12
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-12-18 08:06:58 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def show
+    user = User.find(params[:id])
     @nickname = current_user.nickname
     # @study_scores = StudyScores.where(user_id: current_user.id)
   end
