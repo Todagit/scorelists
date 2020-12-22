485675418aa41ad72da9a3523d93586132dff16a
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-12-22 10:53:12 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,5 @@ Rails.application.routes.draw do
   resources :scores, only: [:index, :new]
   resources :study_scores, only: [:index, :new, :create, :edit]
   resources :toeic_scores, only: [:index, :new, :create, :edit]
-  # resources :toeic_scores, only: [:index, :new, :create, :edit] do
-  #   member do
-  #     get 'new', to: 'toeic_scores#new'
-  #   end
-  # end
 
 end
