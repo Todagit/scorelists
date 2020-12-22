1a03b7e87024244827059499364af7ffeb45266a
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-12-22 10:53:01 +0900

    Update toeic_scores_controller.rb

--- app/controllers/toeic_scores_controller.rb
+++ app/controllers/toeic_scores_controller.rb
@@ -31,7 +31,7 @@ class ToeicScoresController < ApplicationController
     if @toeic_score.valid?
       @toeic_score.save
       flash[:notice] = 'スコアを登録しました'
-      redirect_to user_path
+      redirect_to root_path
     else
       # binding.pry
       flash.now[:alert] = '必須項目を入力してください'
