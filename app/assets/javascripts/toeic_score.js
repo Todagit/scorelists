48da987a7a615ff101bcbb9418a77c09e0f8412d
Yutaro-Toda <yutaro.toda1@gmail.com>
2020-12-27 11:12:12 +0900

    create calculation of toeic score
    リスニングとリーディングのスコアを入力したら、合計スコアが表示されるようにしました

--- app/assets/javascripts/toeic_score.js
+++ app/assets/javascripts/toeic_score.js
@@ -7,7 +7,7 @@ $(function(){
 
     let reading_score = $( '.f__field__score__reading_score' ).val();
     
-    let total_score = parseFloat(listening_score + reading_score);
+    let total_score = eval(listening_score) + eval(reading_score);
     
     $('.new__studyscore__total__score').html(total_score);
     
