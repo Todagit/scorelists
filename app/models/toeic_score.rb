a0de757d3694d1e8f9204e76e5bff2d67b8ae43e
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-02 07:57:30 +0900

    Update toeic_score.rb

--- app/models/toeic_score.rb
+++ app/models/toeic_score.rb
@@ -4,10 +4,13 @@ class ToeicScore < ApplicationRecord
 
   validates  :test_date,              presence: true
   validates  :administration_number,  presence: true
-  validates  :listening_score,        presence: true
-  validates  :reading_score,          presence: true
-  validates  :listening_score,        numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
-  validates  :reading_score,          numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
+  validates  :listening_score,        presence: true, 
+                                      numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
+  validates  :reading_score,          presence: true,
+                                      numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
+
+  # validates  :listening_score,        numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
+  # validates  :reading_score,          numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
   
   before_save do
     self.total_score = listening_score + reading_score
