73ae4d1616c2f37514e1de676f31fa0b12240b40
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-03 21:01:04 +0900

    Update toeic_score.rb

--- app/models/toeic_score.rb
+++ app/models/toeic_score.rb
@@ -5,9 +5,9 @@ class ToeicScore < ApplicationRecord
   validates  :test_date,              presence: true
   validates  :administration_number,  presence: true
   validates  :listening_score,        presence: true, 
-                                      numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
+                                      numericality: { only_integer: true, greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
   validates  :reading_score,          presence: true,
-                                      numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
+                                      numericality: { only_integer: true, greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
 
   # validates  :listening_score,        numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
   # validates  :reading_score,          numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
