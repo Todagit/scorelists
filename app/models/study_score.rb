9c0b1909bacb0c839305ef0c7bca25432c06dd18
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-12 10:21:33 +0900

    Update study_score.rb

--- app/models/study_score.rb
+++ app/models/study_score.rb
@@ -3,5 +3,10 @@ class StudyScore < ApplicationRecord
   belongs_to :user
 
   validates  :test_name, presence: true
-  
+  validates  :japanese, 
+             numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 200 }
+             
+  validates  :math1a, :math2b, :worldhistory, :japanesehistory, :modernsociety, :geography, 
+             :chemistry, :physics, :english_l, :english_r, :other1, :other2,
+             numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
