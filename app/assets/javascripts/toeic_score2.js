815d315e054fd4ad2cd91f0d06ecaafff4a60432
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-16 08:59:40 +0900

    Create toeic_score2.js

---
+++ app/assets/javascripts/toeic_score2.js
@@ -0,0 +1,21 @@
+$(function(){
+  $( document ).on( 'change keyup', '.f__field__score__listening_score', '.f__field__score__reading_score', function(e){ 
+    
+    e.preventDefault();
+
+    const input1 = document.querySelector(".f__field__score__listening_score");
+
+    const input2 = document.querySelector(".f__field__score__reading_score");
+
+    // if (input1 % 5 === 0 && input2 % 5 === 0){
+
+    //   $('.f__submit__btn').prop('disabled', false);
+
+    // } else {
+
+    //   $('.f__submit__btn').prop('disabled', true);
+
+    // }
+
+  });
+});
