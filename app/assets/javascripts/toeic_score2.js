d19ba87d1dc58529ecee6a87f70f3b8cbda466b9
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-19 12:52:11 +0900

    Update toeic_score2.js

--- app/assets/javascripts/toeic_score2.js
+++ app/assets/javascripts/toeic_score2.js
@@ -1,5 +1,5 @@
 $(function(){
-  $( document ).on( 'change keyup', '.f__field__score__listening_score', '.f__field__score__reading_score', function(e){ 
+  $( document ).on( 'change keyup', '.f__field', function(e){ 
     
     e.preventDefault();
 
@@ -7,13 +7,13 @@ $(function(){
 
     const input2 = document.querySelector(".f__field__score__reading_score");
 
-    if (input1 % 5 === 0 && input2 % 5 === 0){
+    if (input1 % 5 == 0 && input2 % 5 == 0){
 
-      $('.f__submit__btn').attr('disabled', false);
+      $('.f__submit__btn').prop("disabled", false);
 
     } else {
 
-      $('.f__submit__btn').attr('disabled', true);
+      $('.f__submit__btn').prop("disabled", true);
 
     }
 
