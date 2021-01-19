a77aba5ff93c29ab684a3422d75fa0a6013f29b2
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-20 00:37:50 +0900

    Update toeic_score.js

--- app/assets/javascripts/toeic_score.js
+++ app/assets/javascripts/toeic_score.js
@@ -3,6 +3,8 @@ $(function(){
     
     e.preventDefault();
 
+    // リスニングとリーディングのスコアを動的に足し算して表示する
+
     let listening_score = $( '.f__field__score__listening_score' ).val();
 
     let reading_score = $( '.f__field__score__reading_score' ).val();
