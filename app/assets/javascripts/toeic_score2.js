a46de2b4d997196066caed37d6d49cfd80ce6208
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-01-20 00:32:43 +0900

    Update toeic_score2.js
    TOEIC SCOREの入力フォームの中で、スコアの部分が５の倍数でない不正なスコアのときは「登録する」ボタンが押せないようにしました。

--- app/assets/javascripts/toeic_score2.js
+++ app/assets/javascripts/toeic_score2.js
@@ -3,9 +3,13 @@ $(function(){
     
     e.preventDefault();
 
-    const input1 = document.querySelector(".f__field__score__listening_score");
+    // const input1 = document.querySelector(".f__field__score__listening_score");
 
-    const input2 = document.querySelector(".f__field__score__reading_score");
+    // const input2 = document.querySelector(".f__field__score__reading_score");
+
+    const input1 = $( '.f__field__score__listening_score' ).val();
+
+    const input2 = $( '.f__field__score__reading_score' ).val();
 
     if (input1 % 5 == 0 && input2 % 5 == 0){
 
