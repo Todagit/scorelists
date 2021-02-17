189061bfb99bb525cdccfba134b2a6ba32c194f6
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-02-17 19:02:05 +0900

    Update toeic_score.rb

--- app/models/toeic_score.rb
+++ app/models/toeic_score.rb
@@ -11,15 +11,6 @@ class ToeicScore < ApplicationRecord
                                       numericality: { only_integer: true, greater_than_or_equal_to: 5, less_than_or_equal_to: 495 }
   
   before_save do
-
-    # if listening_score % 5 != 0 || reading_score % 5 != 0
-    #   flash.now[:alert] = '正しいスコアを入力してください'
-    #   @toeic_score = ToeicScore.new(toeic_score_params)
-    #   render :new
-    # else
-    #   self.total_score = listening_score + reading_score
-    # end
-
     self.total_score = listening_score + reading_score
   end
 
