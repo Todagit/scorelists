ab6a5bf5d3f20fc22f66ed5c227e1608e079dcd3
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-04-08 08:17:01 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "study_scores#index"
+  root to: "scores#index"
   resources :users
   resources :scores, only: [:index, :new]
   resources :study_scores, only: [:index, :new, :create, :edit]
