a7c5221f72c70c19fadad9425882c11121650d38
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-04-21 09:46:32 +0900

    Update studyscore.js

--- app/assets/javascripts/studyscore.js
+++ app/assets/javascripts/studyscore.js
@@ -3,8 +3,17 @@ $(function(){
     
     e.preventDefault();
 
+    // 各教科の点数を動的に足し算して表示する
+
+    let japanese_score = $( '.f__studyscore__field__japanese' ).val();
+
+    let math1a_score = $( '.f__studyscore__field__math1a' ).val();
+
+    // ここに随時付け足す
     
-    
+    let total_score = eval(japanese_score) + eval (math1a_score);
+
+    $('.new__studyscore__total__score').html(total_score);
 
   });
 });
