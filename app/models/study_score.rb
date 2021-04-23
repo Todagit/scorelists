bfbcfc775320aae5546c2c96dde4d3d79e2584aa
Yutaro-Toda <yutaro.toda1@gmail.com>
2021-04-23 19:07:25 +0900

    Update study_score.rb

--- app/models/study_score.rb
+++ app/models/study_score.rb
@@ -12,7 +12,19 @@ class StudyScore < ApplicationRecord
              numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 
   before_save do
-    self.total_score = japanese + math1a + math2b + worldhistory + japanesehistory + modernsociety + geography + chemistry + physics + english_l + english_r + other1 + other2
+    self.total_score = japanese 
+                      + math1a 
+                      + math2b 
+                      + worldhistory 
+                      + japanesehistory 
+                      + modernsociety 
+                      + geography 
+                      + chemistry 
+                      + physics 
+                      + english_l 
+                      + english_r 
+                      + other1 
+                      + other2
   end
 
 end
